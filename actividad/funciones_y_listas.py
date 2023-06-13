5dc4cee92d506ba75ef2645a1b3e654ec2d6040b
Faaian <fabi.loyola@duocuc.cl>
2023-06-13 03:09:38 -0400

    Update funciones_y_listas.py

--- actividad/funciones_y_listas.py
+++ actividad/funciones_y_listas.py
@@ -1,10 +1,13 @@
 import numpy as np
+import os
 menu = """
 1- Ver ubicaciones disponibles.
 2- Comprar ticket.
 3- Ver compras realizadas.
 4- Salir.
 """
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def seleccionar_opcion():
     opcion_valida = False
@@ -17,7 +20,29 @@ def seleccionar_opcion():
             opcion_valida = True
     return opcion
 
+def lista_comprar(ubicaciones):
+    print("Ubicaciones disponibles:")
+    print("   1 2 3 4 5 6 7 8 9 10")
+    for fila, asientos in ubicaciones.items():
+        print(fila, end=" ")
+        for asiento in asientos:
+            print(asiento, end=" ")
+        print()
 
+def mostrar_ubicaciones_disponibles(ubicaciones):
+    filas = len(ubicaciones)
+    columnas = len(ubicaciones["A"])
+
+    matriz = np.empty((filas, columnas), dtype=str)
+
+    for i, (fila, columnas) in enumerate(ubicaciones.items()):
+        for j, valor in columnas.items():
+            if valor == "O":
+                matriz[i, int(j) - 1] = "O"
+            else:
+                matriz[i, int(j) - 1] = "X"
+
+    print(matriz)
 
 ubicaciones = {
     "A":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
@@ -39,20 +64,37 @@ ubicaciones = {
     "Q":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
     "R":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
     "S":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
+    "T":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
+    "U":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
+    "U":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
+    "W":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
+    "X":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
+    "Y":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"},
+    "Z":{"1":"O", "2": "O", "3": "O", "4": "O", "5": "O", "6": "O", "7": "O", "8": "O", "9": "O", "10": "O"}
 }
 
 
 while True:
+    limpiar_pantalla()
     opcion_selecciona = seleccionar_opcion()
     if opcion_selecciona == "1":
-        print ("Ver ubicaciones disponibles.")
+        limpiar_pantalla()
+        print ("Ver ubicaciones disponibles:")
+        mostrar_ubicaciones_disponibles(ubicaciones)
+        input("Presione Enter para volver al menú principal...")
         
     elif opcion_selecciona == "2":
+        limpiar_pantalla()
         print ("Comprar tickets.") 
+        input("Presione Enter para volver al menú principal...")
         
     elif opcion_selecciona == "3":
+       limpiar_pantalla()
        print ("Ver compras realizadas.")
+       input("Presione Enter para volver al menú principal...")
     elif opcion_selecciona == "4":
             break
-
-    print ("Fin ejercicio")      
+    else:
+        print("Opción inválida. Por favor, ingrese una opción válida.")
+        input("Presione Enter para continuar...")
+     
